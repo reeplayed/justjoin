b62d39923abb3372c374c5b978fa3e408169881f
reeplayed98 <lukasz.borkowski8991@gmail.com>
2020-06-15 09:49:01 +0200

    'gg'

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = 'https://justjoin.herokuapp.com';
+export const baseURL = 'https://justjoinv2.herokuapp.com';
 
 export default axios.create({
   baseURL,
